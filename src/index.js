cde799e3d70297da7af9e12155a74c29daaa2162
abdoutony <abdouaallb@gmail.com>
2024-03-24 10:48:23 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch';
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
+import { createClient } from '@supabase/supabase-js'
+import {SupabaseVectorStore} from "langchain/vectorstores/supabase"
+import {OpenAIEmbeddings} from "langchain/embeddings/openai"
+require("dotenv").config()
+
+
+
+async function splitText(url){
+
+    const response = await fetch(url);
+    const text = await response.text();
+    const splitter = new RecursiveCharacterTextSplitter({chunkSize: 500,
+        chunkOverlap: 50,
+        lengthFunction: (text) =>
+        text.length,
+        separators: ["\n\n", "\n", " ", ""],
+    });
+    const docs = await splitter.createDocuments([text]);
+    // console.log(docs);
+    const sbPUrl = process.env.SUPABASE_PROJECT_URL
+    const sbApiKey = process.env.SUPABASE_API_KEY
+    const openaiApikey = process.env.OPEN_AI_API_KEY
+    const client = createClient(sbPUrl, sbApiKey)
+    await SupabaseVectorStore.fromDocuments(
+        docs,
+        new OpenAIEmbeddings({openAIApiKey: openaiApikey}),
+        {client: client,
+        tableName: 'documents'
+        }
+    )
+
+}
+
+// splitText('https://www.gutenberg.org/files/1342/1342-0.txt')
